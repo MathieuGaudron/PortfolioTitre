86ac6b5809518c75dbbe4324572bf22580baeb4a
mathieu <mathieu.gaudron@epitech.eu>
2025-03-19 10:06:00 +0100

    [FIX] sitemap

---
+++ portfolio-mathieu/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://mathieu-gaudron-devfolio.vercel.app",
+  generateRobotsTxt: true,
+  sitemapSize: 5000, 
+};
--- portfolio-mathieu/next-stiemap.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    siteUrl: "https://mathieu-gaudron-devfolio.vercel.app",
-    generateRobotsTxt: true,
-  };
-  
