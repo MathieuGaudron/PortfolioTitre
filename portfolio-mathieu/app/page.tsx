93967d4b0d67885f92d6a0d0154f8acdaea9bafa
mathieu <mathieu.gaudron@epitech.eu>
2025-03-19 10:25:37 +0100

    [ADD] balise meta google

--- portfolio-mathieu/app/page.tsx
+++ portfolio-mathieu/app/page.tsx
@@ -4,15 +4,24 @@ import Stack from "./components/Stack";
 // import Card from "./components/Card";
 import Cards from "./components/Cards";
 import ContactForm from "./components/ContactForm";
-
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="_XOV1wQskjFLFy_gqsuPGhhjQAP-igooA1ZJqzlQB48"
+        />
+      </Head>
       <Navbar />
 
       <main className="relative bg-custom-radial text-white min-h-screen flex flex-col items-center font-geist">
-        <div className="opacity-0 scale-95 animate-scale-in w-full" id="accueil">
+        <div
+          className="opacity-0 scale-95 animate-scale-in w-full"
+          id="accueil"
+        >
           <section>
             <Hero />
           </section>
